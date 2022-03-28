d1b3c5c1ff506cfc833c7d2ff1dd8194d73dc646
Freedom Loisa <kitakayaloisa@gmail.com>
2022-03-28 20:45:34 +0300

    finished styling contact page

--- core/views.py
+++ core/views.py
@@ -1,7 +1,45 @@
 from django.shortcuts import render
+from django.contrib import messages
+from django.core.mail import EmailMultiAlternatives
+from django.conf import settings
+from django.template.loader import render_to_string
 
 # Create your views here.
 
 def home(request):
 
     return render(request, 'core/index.html')
+
+def contact(request):
+
+    if request.method == 'POST':
+
+        name = request.POST.get('name')
+
+        email = request.POST.get('email')
+
+        message = request.POST.get('message')
+
+        context = {
+            'name': name,
+            'email': email,
+            'message': message,
+        }
+
+        subject = 'New message. Important.'
+
+        from_email = settings.EMAIL_HOST_USER 
+
+        to = settings.RECIPIENT_ADDRESS
+
+        text_content = 'New message from freedom.io. Someone wants to connect.'
+
+        html_content = render_to_string('core/email_notification.html', context)
+
+        msg = EmailMultiAlternatives(subject, text_content, from_email, [to])
+        msg.attach_alternative(html_content, "text/html")
+        msg.send()
+
+        messages.success(request, 'Your message has been sent successfully.')
+
+    return render(request, 'core/contact.html')
--- freedom/settings.py
+++ freedom/settings.py
@@ -145,3 +145,12 @@ MEDIA_URL = '/media/'
 
 # Path where media is stored
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
+# Email configuration
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'loisadevmode@gmail.com'
+EMAIL_HOST_PASSWORD = '#Kitloisa15'
+RECIPIENT_ADDRESS = 'kitakayaloisa@gmail.com'
